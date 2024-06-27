e3129850334569981cf43c1c9a2af6659cad661f
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2024-06-27 09:49:25 -0300

    consertando bugs da questão 1

--- PDS1/lab06-funcoes/contandoDias.c
+++ PDS1/lab06-funcoes/contandoDias.c
@@ -1,3 +1,4 @@
+//01 01 2020 deveria dar 260 e no teste da 261
 #include <stdio.h>
 #define SUCESSO 0
 #define DATA_DIA 17
@@ -7,29 +8,31 @@
 int contaDias(int dia, int mes, int ano){
     int i, resultado=0, diasPassados=0;
     int meses[12]={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    for(i=ano+1;i<=DATA_ANO;i++){
-        if((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0)){
-            printf("366%d\n", i);
+    for(i=ano;i<DATA_ANO;i++){
+        if(((i+1) % 4 == 0 && (i+1) % 100 != 0) || ((i+1) % 400 == 0)){
+            //printf("366%d\n", i+1);
             resultado+=366;
         }else{
-            printf("365%d\n", i);
+            //printf("365%d\n", i+1);
             resultado+=365;
         }
     }
     if((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)){
         meses[1]=29;
-    }else{
-        meses[1]=28;
+        resultado++;
     }
+
     for(i=0;i<mes-1;i++){
         diasPassados+=meses[i];
     }
     diasPassados+=dia;
-    printf("a%d b%d\n", diasPassados, resultado);
+    //printf("a%d b%d\n", diasPassados, resultado);
 
     if(diasPassados<DATA_DIAS_JA_PASSADOS){
+        //printf("menor");
         resultado=resultado+(DATA_DIAS_JA_PASSADOS-diasPassados);
     }else{
+        //printf("maior");
         resultado=resultado-(diasPassados-DATA_DIAS_JA_PASSADOS);
     }
 
@@ -37,7 +40,7 @@ int contaDias(int dia, int mes, int ano){
 }
 
 
-int main(int argc, char**argv[]){
+int main(){
     int dia, mes, ano, numDias;
     while(scanf("%d %d %d", &dia, &mes, &ano)!=EOF){
     numDias=contaDias(dia, mes, ano);
