77a73851264c216511e533549bc052292a3f7801
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2024-07-03 10:26:25 -0300

    fazendo exercício contando letras de arquivo txt

---
+++ PDS1/lab07-arquivos/contaLetras.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SUCESSO 0
+#define TAMANHO_MAX_NOME 50
+int main(int argc, char*argv[]){
+    FILE *arquivoEntrada;
+    const char *nomeArquivo= argv[1];
+    int quantLetras=0;
+    char letraArquivo;
+    arquivoEntrada=fopen(nomeArquivo, "r");
+    
+    if(arquivoEntrada==NULL){
+        //printf("Erro na abertura do arquivo");
+        exit(1);
+    }
+    while(1){
+        letraArquivo=fgetc(arquivoEntrada);
+        if(feof(arquivoEntrada)){
+            break;
+        }
+        if(letraArquivo=='a'){
+            quantLetras++;
+        }
+    }
+    printf("%d\n", quantLetras);
+    fclose(arquivoEntrada);
+    return SUCESSO;
+}
